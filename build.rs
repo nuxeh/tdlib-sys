462ecea7cb5e764828403301a238a90aff8e1796
Ed Cragg <drq.11235@gmail.com>
2020-04-18 21:56:24 +0100

    Attempt build

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use cmake;
 
-let dst = cmake::build("libfoo");
+fn main() {
+    let dst = cmake::build("td");
 
-println!("cargo:rustc-link-search=native={}", dst.display());
-println!("cargo:rustc-link-lib=static=foo");
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=foo");
+}
