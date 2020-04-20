78f053a4b428a31fe8453410016dc808bbe02f58
Ed Cragg <drq.11235@gmail.com>
2020-04-20 23:50:54 +0100

    Implement bindgen generation

--- tdlib-bindgen/build.rs
+++ tdlib-bindgen/build.rs
@@ -2,16 +2,15 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let tdlib_header = env::var("DEP_TDJSON_INCLUDE")
+        .map(PathBuf::from)
+        .expect("Can't access header from environment")
+	.join("tdjson.h");
+
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header(header_file)
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
+        .header(tdlib_header.to_string_lossy())
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
